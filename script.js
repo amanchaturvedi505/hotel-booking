a374c00f274471cb09ae9191a642d9e806abb317
amanchaturvedi505 <amanchaturvedi2791999@gmail.com>
2023-11-15 19:35:15 +0530

    Project completed

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('bookingForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Your booking form submission logic goes here
+    alert('Booking submitted!'); // Example alert, replace with your logic
+});
